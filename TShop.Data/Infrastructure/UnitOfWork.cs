d968bb50922357f87c2445e5e7c060e3be28dec8
DESKTOP-PRPRNRE\HP <lightstar.tqt@gmail.com>
2018-03-04 02:58:02 +0700

    adjust coding convention

--- TShop.Data/Infrastructure/UnitOfWork.cs
+++ TShop.Data/Infrastructure/UnitOfWork.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TShop.Data.Infrastructure
+﻿namespace TShop.Data.Infrastructure
 {
-    public class UnitOfWork : IUnitOfWork 
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly IDbFactory dbFactory;
         private TShopDbContext dbContext;
+
         public UnitOfWork(IDbFactory dbFactory)
         {
             this.dbFactory = dbFactory;
         }
+
         public TShopDbContext DbContext
         {
             get { return dbContext ?? (dbContext = dbFactory.Init()); }
         }
+
         public void Commit()
         {
             DbContext.SaveChanges();
