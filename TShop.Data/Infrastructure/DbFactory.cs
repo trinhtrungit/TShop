b6140f460dd6d679bc107767106574d3a1ee754a
DESKTOP-PRPRNRE\HP <lightstar.tqt@gmail.com>
2018-03-04 02:54:05 +0700

    add entity framework for data in infrastructure folder.

---
+++ TShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TShopDbContext dbContext;
+        // define method from interface
+        public TShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TShopDbContext());
+        }
+        // overrid from parent class
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
---
+++ TShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool isDisposing)
+        {
+            if (!isDisposed && isDisposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        // declare to child class override
+        protected virtual void DisposeCore()
+        {
+            // Do something
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+    }
+}
